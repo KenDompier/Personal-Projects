f60f30361a5167c05abe15884c0c492ed6319af1
Ken <80441717+KenDompier@users.noreply.github.com>
2021-10-02 16:02:43 -0500

    Update randomvids.py

--- Random Video Program/randomvids.py
+++ Random Video Program/randomvids.py
@@ -5,7 +5,7 @@ def youtube():
   import string
   import random
   count = 1 #if it goes beyond one, the program outputs videos with similar titles. so its set to 1
-  API_KEY = #your key here
+  API_KEY = 'your key here'
   random = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(3))
 
   urlData = "https://www.googleapis.com/youtube/v3/search?publishedBefore=2012-12-31T00:00:00.0Z&videoDuration=short&key={}&maxResults={}&part=snippet&type=video&q={}".format(API_KEY,count,random)
